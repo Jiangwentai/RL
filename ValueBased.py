5a589eab71010a53ffe55fa8445395a6df8a7b4d
Jiangwentai <68869967+Jiangwentai@users.noreply.github.com>
2020-07-31 02:58:03 +0800

    Update ValueBased.py
    初始化权重为1 确定收敛的版本

--- ValueBased.py
+++ ValueBased.py
@@ -10,7 +10,6 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import copy
 
-learning_rate = 0.00001
 epsilon = 1  # exploration rate
 epsilon_min = 0.01
 epsilon_decay = 0.995
@@ -24,36 +23,41 @@ minibatch=deque(maxlen=200000)
 env = gym.make('CartPole-v1')
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
+
+
+learning_rate=0.000000001
+
+starter_learning_rate = 0.000000001
+learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(starter_learning_rate, decay_steps=110,decay_rate=0.96, staircase=True)
+
+
+
 model = tf.keras.models.Sequential([
 
-tf.keras.layers.Dense(32, input_shape=(4,), activation='relu'),
-  tf.keras.layers.Dense(64, activation='relu'),
-    # tf.keras.layers.Dense(64, activation='relu'),
+tf.keras.layers.Dense(16,kernel_initializer=initializers.ones, input_shape=(4,), activation=tf.nn.leaky_relu),
+  tf.keras.layers.Dense(16,kernel_initializer=initializers.ones, activation=tf.nn.leaky_relu),
 
-
-    # tf.keras.layers.Dense(256, activation='relu'),
+    # tf.keras.layers.Dense(64,kernel_initializer=initializers.glorot_normal, activation='relu'),
 
     tf.keras.layers.Dense(2, activation='linear')
 
 ])
 
-model.compile(loss='mse',
-                    optimizer=Adam(lr=learning_rate))
+
 
 targetmodel = tf.keras.models.Sequential([
 
-tf.keras.layers.Dense(32, input_shape=(4,), activation='relu'),
-  tf.keras.layers.Dense(64, activation='relu'),
-    # tf.keras.layers.Dense(64, activation='relu'),
+tf.keras.layers.Dense(16,kernel_initializer=initializers.ones, input_shape=(4,), activation=tf.nn.leaky_relu),
+  tf.keras.layers.Dense(16,kernel_initializer=initializers.ones, activation=tf.nn.leaky_relu),
 
-
-
-    # tf.keras.layers.Dense(256, activation='relu'),
+    # tf.keras.layers.Dense(64,kernel_initializer=initializers.glorot_normal, activation='relu'),
 
     tf.keras.layers.Dense(2, activation='linear')
 
 ])
-# targetmodel.set_weights(model.get_weights())
+targetmodel.set_weights(model.get_weights())
 #
 opt=tf.optimizers.Adam(learning_rate=learning_rate)
 finallist=[]
@@ -79,7 +83,7 @@ for epoch in range(2000000):
 
         next_state, reward, done, _ = env.step(at)
         next_state=np.reshape(next_state,[1,4])
-        minibatch.append((state,next_state,at,reward*0.01,done))
+        minibatch.append((state,next_state,at,reward,done))
         # with tf.GradientTape() as tape:
         #     qt = model(state)[0][at]
         #     yt = reward + np.multiply(gamma, np.amax(targetmodel(next_state)[0]))
@@ -94,17 +98,16 @@ for epoch in range(2000000):
         c=c+1
         if done ==True:
             if len(minibatch) >129:
-                for _state, _next_state, _at, _reward, _done in random.sample(minibatch, 32):
-
+                for _state, _next_state, _at, _reward, _done in random.sample(minibatch, 128):
                     with tf.GradientTape() as tape:
                         qt = model(_state)[0][_at]
-                        yt = tf.convert_to_tensor(_reward*0.01) + np.multiply(gamma, np.amax(targetmodel(_next_state)[0]))
+                        yt = tf.convert_to_tensor(_reward) + np.multiply(gamma, np.amax(targetmodel(_next_state)[0]))
                         y_pred = tf.square(tf.math.subtract(yt,qt))
 
                     grad = tape.gradient(y_pred, model.trainable_variables)
                     grad = np.multiply(grad, 0.5)
                     opt.apply_gradients(zip(grad, model.trainable_variables))
-            if c!=0 and c%1000==0:
+            if epoch!=0 and epoch%1==0:
                 targetmodel.set_weights(model.get_weights())
             break
 
